bdbd49eb9dd76d11a44ced3c0b543033df03ad8f
Jassu323 <Jassu323@gmail.com>
2026-06-25 22:33:22 -0500

    Exp from Catching Pokémon
    Catching Pokémon yields exp like Gens 5+.  Tutorial: https://github.com/pret/pokecrystal/wiki/Gain-experience-from-catching-Pokémon

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2351,6 +2351,8 @@ UpdateBattleStateAndExperienceAfterEnemyFaint:
 	ld a, [wBattleResult]
 	and BATTLERESULT_BITMASK
 	ld [wBattleResult], a ; WIN
+	; fallthrough
+ApplyExperienceAfterEnemyCaught:
 	call IsAnyMonHoldingExpShare
 	jr z, .skip_exp
 	ld hl, wEnemyMonBaseStats
