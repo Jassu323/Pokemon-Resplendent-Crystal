49af1b324389a8c9e7f533728e5d4093de7d6056
Jassu323 <Jassu323@gmail.com>
2026-06-29 22:27:14 -0500

    Route 29 fix
    Accidentally let testing changes through.

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1280,32 +1280,31 @@ JohtoGrassWildMons:
 
 	def_grass_wildmons ROUTE_29
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
-	; TEMP TEST: Anorith Route 29 encounter hook
 	; morn
 	; encounterPercent, SPECIES, minLevel, maxLevel
-	dbwbb 30, ANORITH,       2,  2
-	dbwbb 30, ANORITH,       2,  2
-	dbwbb 20, ANORITH,       3,  3
-	dbwbb 10, ANORITH,       3,  3
-	dbwbb  5, ANORITH,       2,  2
-	dbwbb  4, ANORITH,       3,  3
-	dbwbb  1, ANORITH,       3,  3
+	dbwbb 30, PIDGEY,        2,  2
+	dbwbb 30, SENTRET,       2,  2
+	dbwbb 20, PIDGEY,        3,  3
+	dbwbb 10, SENTRET,       3,  3
+	dbwbb  5, RATTATA,       2,  2
+	dbwbb  4, HOPPIP,        3,  3
+	dbwbb  1, HOPPIP,        3,  3
 	; day
-	dbwbb 30, ANORITH,       2,  2
-	dbwbb 30, ANORITH,       2,  2
-	dbwbb 20, ANORITH,       3,  3
-	dbwbb 10, ANORITH,       3,  3
-	dbwbb  5, ANORITH,       2,  2
-	dbwbb  4, ANORITH,       3,  3
-	dbwbb  1, ANORITH,       3,  3
+	dbwbb 30, PIDGEY,        2,  2
+	dbwbb 30, SENTRET,       2,  2
+	dbwbb 20, PIDGEY,        3,  3
+	dbwbb 10, SENTRET,       3,  3
+	dbwbb  5, RATTATA,       2,  2
+	dbwbb  4, HOPPIP,        3,  3
+	dbwbb  1, HOPPIP,        3,  3
 	; nite
-	dbwbb 30, ANORITH,       2,  2
-	dbwbb 30, ANORITH,       2,  2
-	dbwbb 20, ANORITH,       3,  3
-	dbwbb 10, ANORITH,       3,  3
-	dbwbb  5, ANORITH,       2,  2
-	dbwbb  4, ANORITH,       3,  3
-	dbwbb  1, ANORITH,       3,  3
+	dbwbb 30, HOOTHOOT,      2,  2
+	dbwbb 30, RATTATA,       2,  2
+	dbwbb 20, HOOTHOOT,      3,  3
+	dbwbb 10, RATTATA,       3,  3
+	dbwbb  5, RATTATA,       2,  2
+	dbwbb  4, HOOTHOOT,      3,  3
+	dbwbb  1, HOOTHOOT,      3,  3
 	end_grass_wildmons
 
 	def_grass_wildmons ROUTE_30
