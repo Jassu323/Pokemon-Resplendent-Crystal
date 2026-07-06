d949c10c3025473c8cc1c7d87118a480c8658934
Jassu323 <Jassu323@gmail.com>
2026-07-05 22:40:23 -0500

    Dragonair
    Replaced Dragonair's sprite.  Modified the pink shiny palette index to use Dratini's pink so it wouldn't look horribly washed out.

--- data/pokemon/menu_icons.asm
+++ data/pokemon/menu_icons.asm
@@ -154,7 +154,7 @@ MonMenuIcons:
 	mon_menu_icon ZapdosIcon,         BROWN,  RED    ; ZAPDOS
 	mon_menu_icon MoltresIcon,        RED,    PINK   ; MOLTRES
 	mon_menu_icon DratiniIcon,        BLUE,   PINK   ; DRATINI
-	mon_menu_icon DragonairIcon,      BLUE,   PURPLE ; DRAGONAIR
+	mon_menu_icon DragonairIcon,      BLUE,   PINK   ; DRAGONAIR
 	mon_menu_icon DragoniteIcon,      RED,    GREEN  ; DRAGONITE
 	mon_menu_icon MewtwoIcon,         PURPLE, GREEN  ; MEWTWO
 	mon_menu_icon MewIcon,            PINK,   BLUE   ; MEW
